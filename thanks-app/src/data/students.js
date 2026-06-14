71731155abe81ae410d6b68e12c48424b47913fe
Butenkite <butenkite@gmail.com>
2026-06-13 23:49:56 -0700

    Niko Image

--- thanks-app/src/data/students.js
+++ thanks-app/src/data/students.js
@@ -20,54 +20,68 @@
 
 const students = [
   {
-    name: 'Rebecca Riffle',
-    blurb: 'Cohort 20 | From teaching classical Latin to building innovative software, Rebecca can do it all.',
-    linkedin: 'https://www.linkedin.com/in/rebecca-riffle/',
-    photo: '/photos/students/rebeccariffle.jpg',
+    name: "Rebecca Riffle",
+    blurb:
+      "Cohort 20 | From teaching classical Latin to building innovative software, Rebecca can do it all.",
+    linkedin: "https://www.linkedin.com/in/rebecca-riffle/",
+    photo: "/photos/students/rebeccariffle.jpg",
   },
   // Add yourself below this line ↓
   {
-    name: 'Emily Menken',
-    blurb: 'Cohort 21 | Passionate about learning, animals, and creating things that enhance other people’s lives.',
-    linkedin: 'https://www.linkedin.com/in/emily-menken/',
-    photo: '/photos/students/EmilyMenken.jpg',
+    name: "Emily Menken",
+    blurb:
+      "Cohort 21 | Passionate about learning, animals, and creating things that enhance other people’s lives.",
+    linkedin: "https://www.linkedin.com/in/emily-menken/",
+    photo: "/photos/students/EmilyMenken.jpg",
   },
   {
-    name: 'Brady Leggett',
-    blurb: 'Cohort 21 | Excited to learn new skills and wanting to build tools that make the world a better place.',
-    linkedin: 'https://www.linkedin.com/in/brady-leggett-107130293/',
-    photo: '/photos/students/BradyLeggett.jpg',
+    name: "Brady Leggett",
+    blurb:
+      "Cohort 21 | Excited to learn new skills and wanting to build tools that make the world a better place.",
+    linkedin: "https://www.linkedin.com/in/brady-leggett-107130293/",
+    photo: "/photos/students/BradyLeggett.jpg",
   },
   {
-    name: 'Tim Williams',
-    blurb: 'Cohort 21 | Enjoy watching movies, spending time with family, and learning more about Software',
-    linkedin: 'https://www.linkedin.com/in/timothy-williams-hire/',
-    photo: '/photos/students/TimWilliams.jpg',
+    name: "Tim Williams",
+    blurb:
+      "Cohort 21 | Enjoy watching movies, spending time with family, and learning more about Software",
+    linkedin: "https://www.linkedin.com/in/timothy-williams-hire/",
+    photo: "/photos/students/TimWilliams.jpg",
   },
   {
-    name: 'Abdirashid Ahmed',
-    blurb: 'Cohort 21 | At home refactoring code at 2am or getting lost in a city where no one speaks my language.',
-    linkedin: 'https://www.linkedin.com/in/abdirashid1',
-    photo: '/photos/students/abdirashidAhmed.jpg'
+    name: "Abdirashid Ahmed",
+    blurb:
+      "Cohort 21 | At home refactoring code at 2am or getting lost in a city where no one speaks my language.",
+    linkedin: "https://www.linkedin.com/in/abdirashid1",
+    photo: "/photos/students/abdirashidAhmed.jpg",
   },
   {
-    name: 'Xavier Bradley',
-    blurb: 'Cohort 21 | Learning every day and every night to improve my skills and help those around me.',
-    linkedin: 'https://www.linkedin.com/in/xavier-bradley-4bb239307/',
-    photo: '/photos/students/XavierBradley.jpg'
+    name: "Xavier Bradley",
+    blurb:
+      "Cohort 21 | Learning every day and every night to improve my skills and help those around me.",
+    linkedin: "https://www.linkedin.com/in/xavier-bradley-4bb239307/",
+    photo: "/photos/students/XavierBradley.jpg",
   },
   {
-    name: 'Raymond Marx',
-    blurb: 'Cohort 20 | Self-taught, across areas such as formal mathematics, philosophy, and various others, looking to integrate multiple schools of thought to the research and engineering process and create new ideas.',
-    linkedin: 'https://www.linkedin.com/in/rmarx1456/',
-    photo: null
+    name: "Raymond Marx",
+    blurb:
+      "Cohort 20 | Self-taught, across areas such as formal mathematics, philosophy, and various others, looking to integrate multiple schools of thought to the research and engineering process and create new ideas.",
+    linkedin: "https://www.linkedin.com/in/rmarx1456/",
+    photo: null,
   },
   {
-    name: 'Augy Markham',
-    blurb: "Cohort 20 | Design and user experience is my passion. Using my graphic design background and full-stack skills, I will obsessively plan any project I'm given.",
-    linkedin: 'https://www.linkedin.com/in/augy-markham/',
-    photo: '/photos/students/augymarkham.jpg',
+    name: "Augy Markham",
+    blurb:
+      "Cohort 20 | Design and user experience is my passion. Using my graphic design background and full-stack skills, I will obsessively plan any project I'm given.",
+    linkedin: "https://www.linkedin.com/in/augy-markham/",
+    photo: "/photos/students/augymarkham.jpg",
   },
-]
+  {
+    name: "Niko Butenko",
+    blurb: "Cohort 21 | Trying to be better - People Person and Dreamer.",
+    linkedin: "https://www.linkedin.com/in/nick-butenko-7a875722a/",
+    photo: "/photos/students/NikoButenko.jpg",
+  },
+];
 
-export default students
+export default students;
